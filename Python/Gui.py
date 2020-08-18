af8f7c2b32a51c36e235a9bd4f6a729af1fe2749
apanisile <40339991+apanisile@users.noreply.github.com>
2020-08-19 00:55:17 +0100

    Update Gui.py

--- Python/Gui.py
+++ Python/Gui.py
@@ -28,8 +28,9 @@ def convert():
   
 
 HEIGHT = 300
-WIDTH = 800
+WIDTH = 550
 root = tk.Tk()
+root.title("Weight converter")
 
 canvas = tk.Canvas(root, height= HEIGHT, width= WIDTH)
 canvas.pack()
@@ -38,32 +39,34 @@ canvas.pack()
 labelframe = tk.LabelFrame(root, text="Weight converter", bg='#80c1ff')
 labelframe.place(relx=0.06, rely=0.01, relheight= 0.8, relwidth = 0.8)
 
-label = tk.Label(labelframe, text = "Enter your weight:")
+#Create the button widget
+button = tk.Button(labelframe, text="Check weight", command = convert)
+button.pack(side='bottom')
 
+label = tk.Label(labelframe, text = "Enter your weight in kg:")
+entry2_value = StringVar() 
+entry2 = Entry(labelframe, textvariable = entry2_value) 
 entry3 = Label(labelframe, text = 'Gram') 
 entry4 = Label(labelframe, text = 'Pounds') 
 entry5 = Label(labelframe, text = 'Ounce') 
 
-#Create the button widget
-button = tk.Button(labelframe, text="Check weight", command = convert)
+
+
 
 #Empty text boxes to be filled
 text1 = Text(labelframe, height = 1, width = 20) 
 text2 = Text(labelframe, height = 1, width = 20) 
 text3 = Text(labelframe, height = 1, width = 20) 
 
-
 #grids
 label.grid(row=0, column=0)
-
+entry2.grid(row=0, column=1)
 entry3.grid(row=1, column=0) 
 entry4.grid(row=1, column=1) 
 entry5.grid(row=1, column = 2)
 text1.grid(row = 2, column = 0) 
 text2.grid(row = 2, column = 1) 
 text3.grid(row = 2, column = 2) 
-button.grid(row=0, column=2)
-
 
 
 root.mainloop()
